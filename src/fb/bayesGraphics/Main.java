3f80f6ca14c1a07d682e7ccbbe92d30752885103
Matheus Maia <matheus.maia7@hotmail.com>
2019-10-21 11:18:11 -0300

    Create a empty project

---
+++ src/fb/bayesGraphics/Main.java
@@ -0,0 +1,32 @@
+/*Software de correlação de varivaies, utilizando API WEKA
+
+Criado por: Matheus Maia.
+ */
+package fb.bayesGraphics;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        String urlLoader = "/fb/bayesGraphics/view/StartWindow.fxml";
+
+        FXMLLoader loader = new FXMLLoader();
+        Parent root = loader.load(getClass().getResource(urlLoader).openStream());
+
+        Scene scene = new Scene(root);
+        stage.resizableProperty().set(false);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
